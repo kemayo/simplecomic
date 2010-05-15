762577caaa11a0d56e98e3be8ee016497118c017
David Lynch <kemayo@gmail.com>
2010-05-15 03:58:53 -0500

    Adjust datepicker...

--- template/default/admin.js
+++ template/default/admin.js
@@ -7,10 +7,18 @@ $("input.datetime").each(function() {
     var time_input = $('<input>').val(datetime[1]);
     $this.after(time_input).after(date_input);
     date_input.dateinput({format: 'yyyy-mm-dd'});
-    var update_date = function(e) {
+    var update_date = function() {
         $this.val(date_input.val() + ' ' + time_input.val());
     };
     date_input.bind('onHide', update_date);
     time_input.change(update_date);
     $this.closest('form').submit(update_date);
+    $this.prev('label').append($(' <a href="#" title="hide calendar">(x)</a>)').click(function(e) {
+        e.preventDefault();
+        update_date();
+        date_input.remove();
+        time_input.remove();
+        $this.show();
+        $(this).remove();
+    }))
 });
