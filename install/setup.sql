a42142a19acec017438df7fe7924fa298a2f3af0
David Lynch <kemayo@gmail.com>
2010-05-15 02:23:26 -0500

    Database name shouldn't be hardcoded

--- install/setup.sql
+++ install/setup.sql
@@ -1,4 +1,4 @@
- CREATE TABLE `simplecomic`.`comics` (
+ CREATE TABLE `comics` (
 `comicid` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 `title` VARCHAR( 255 ) NOT NULL ,
 `filename` VARCHAR( 255 ) NOT NULL ,
@@ -8,7 +8,7 @@ INDEX `bychapter` ( `chapterid` , `pub_date` ) ,
 INDEX `bydate` ( `pub_date` )
 ) ENGINE = MYISAM CHARACTER SET utf8 COLLATE utf8_general_ci;
 
- CREATE TABLE  `simplecomic`.`comics_text` (
+ CREATE TABLE  `comics_text` (
  `comicid` INT NOT NULL ,
  `description` MEDIUMTEXT NOT NULL ,
  `transcript` MEDIUMTEXT NOT NULL ,
@@ -16,7 +16,7 @@ INDEX `bydate` ( `pub_date` )
  PRIMARY KEY (  `comicid` )
  ) ENGINE = MYISAM CHARACTER SET utf8 COLLATE utf8_general_ci;
 
- CREATE TABLE  `simplecomic`.`chapters` (
+ CREATE TABLE  `chapters` (
  `chapterid` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
  `title` VARCHAR( 255 ) NOT NULL ,
  `slug` VARCHAR( 32 ) NOT NULL ,
@@ -24,19 +24,19 @@ INDEX `bydate` ( `pub_date` )
  `parentid` INT NOT NULL
  ) ENGINE = MYISAM;
 
-CREATE TABLE  `simplecomic`.`chapters_text` (
+CREATE TABLE  `chapters_text` (
  `chapterid` INT NOT NULL ,
  `description` MEDIUMTEXT NOT NULL ,
  PRIMARY KEY (  `chapterid` )
  ) ENGINE = MYISAM CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-CREATE TABLE `simplecomic`.`rants` (
+CREATE TABLE `rants` (
 `rantid` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 `title` VARCHAR( 128 ) NOT NULL ,
 `pub_date` INT NOT NULL
 ) ENGINE = MYISAM ;
 
- CREATE TABLE  `simplecomic`.`rants_text` (
+ CREATE TABLE  `rants_text` (
  `rantid` INT NOT NULL ,
  `text` MEDIUMTEXT NOT NULL ,
  PRIMARY KEY (  `rantid` )
