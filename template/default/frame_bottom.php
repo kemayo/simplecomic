2e49c7ebf976c6ccf22359ccd98124a413ac1961
David Lynch <kemayo@gmail.com>
2010-05-15 02:13:35 -0500

    Broken debug output

--- template/default/frame_bottom.php
+++ template/default/frame_bottom.php
@@ -5,7 +5,7 @@ if(DEBUG) {
     echo "SQL:\n";
     print_r($db->debug_output);
     echo "Other:\n";
-    print_r($page['debug']);
+    print_r($page->get_debug());
     echo '</pre>';
 }
 $page->output_js();
