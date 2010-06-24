8f81be8d923f4d09378092e5fb29a4ddef201cb5
David Lynch <kemayo@gmail.com>
2010-06-23 21:38:31 -0500

    Closing tags...

--- template/default/foot.php
+++ template/default/foot.php
@@ -1,9 +1,9 @@
 </div>
 <div class="navmenu">
     <ul>
-        <li><a href="<?php echo url('archive/');?>">Archive</li>
-        <li><a href="<?php echo url('chapters/');?>">Chapters</li>
-        <li><a href="<?php echo url('rants/');?>">Rants</li>
+        <li><a href="<?php echo url('archive/');?>">Archive</a></li>
+        <li><a href="<?php echo url('chapters/');?>">Chapters</a></li>
+        <li><a href="<?php echo url('rants/');?>">Rants</a></li>
     </ul>
 </div>
 <div class="foot">
